89e451c4313dcff58981f6586913b5b5a7efc6f0
MatygullinEmil <seb.94@mail.ru>
2022-04-19 17:28:04 +0300

    creating a flashlight app

--- HomeWorkNumber1/HomeWorkNumber1/ViewController.swift
+++ HomeWorkNumber1/HomeWorkNumber1/ViewController.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    var count = 0 {
+        didSet {
+            if count == 3 { count = 0}
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
-}
+    @IBAction func buttonPressed() {
+    
+        switch count {
+        case 0:
+            view.backgroundColor = .red
+        case 1:
+            view.backgroundColor = .yellow
+        case 2:
+            view.backgroundColor = .green
+        default:
+            break
+        }
+        count += 1
+        
+        }
+        
+    }
+    
 
