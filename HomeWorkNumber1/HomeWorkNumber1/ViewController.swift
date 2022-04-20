e6b6ea76232127afa8232957f31590c6baf936ad
MatygullinEmil <seb.94@mail.ru>
2022-04-20 18:51:52 +0300

    Adding a code to turn on a real flashlight on the device

--- HomeWorkNumber1/HomeWorkNumber1/ViewController.swift
+++ HomeWorkNumber1/HomeWorkNumber1/ViewController.swift
@@ -6,20 +6,84 @@
 //
 
 import UIKit
+import AVFoundation
 
-final class ViewController: UIViewController {
+   final class ViewController: UIViewController {
+       
+    // MARK: - Properties
     
+    private let button = UIButton(type: .system)
+    private var isRealLightOn = false
     private var count = 0 {
         didSet {
             if count == 3 { count = 0}
         }
     }
     
-    // MARK: - Methods
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
     
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    // MARK: - Methods
+       
     override func viewDidLoad() {
         super.viewDidLoad()
+        createButton()
+        buttonPressed()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+           buttonPressed()
+       }
+    
+    /// Turn On/Off the Device Torch and Switch the Button Title.
+    @objc private func buttonAction() {
+        isRealLightOn.toggle()
+        setupRealLight()
+        
+        var title = ""
+        title = isRealLightOn ? "Torch off": "Torch on"
+        button.setTitle(title, for: .normal)
+    }
+    
+    /// Create Button for control the Device Torch
+    private func createButton() {
+        let widht: CGFloat = 200
+        let height: CGFloat = 50
+        
+        button.frame = CGRect (x: self.view.frame.midX - widht / 2, y: self.view.frame.maxY - self.view.frame.midY/2, width: widht, height: height)
+        button.backgroundColor = .black
+        button.setTitle("Torch on", for: .normal)
+        button.layer.cornerRadius = 25
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(nil, action: #selector(buttonAction), for: .touchUpInside)
+        button.tintColor = .white
+        
+        self.view.addSubview(button)
+    }
+      
+    /// Control the Device Torch Status.
+    private func setupRealLight() {
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+               
+        if let dev = device, dev.hasTorch {
+            do {
+                try dev.lockForConfiguration()
+                dev.torchMode = isRealLightOn ? .on : .off
+                dev.unlockForConfiguration()
+                   } catch {
+                   print(error)
+                   }
+               } else {
+                   print("Device Has No Torch.")
+               }
+           }
+    
+    /// Update the Screen Color
     @IBAction func buttonPressed() {
         switch count {
         case 0:
