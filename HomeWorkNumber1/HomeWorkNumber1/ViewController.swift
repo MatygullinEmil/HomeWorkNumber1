a38456cfb0383435cef58b547fb024341a5eba18
MatygullinEmil <seb.94@mail.ru>
2022-04-21 13:46:14 +0300

    aligned all code by indentation

--- HomeWorkNumber1/HomeWorkNumber1/ViewController.swift
+++ HomeWorkNumber1/HomeWorkNumber1/ViewController.swift
@@ -8,80 +8,81 @@
 import UIKit
 import AVFoundation
 
-    final class ViewController: UIViewController {
-       
+final class ViewController: UIViewController {
+    
     // MARK: - Properties
     
     private var isRealLightOn = false
     private var count = 0 {
-       didSet {
-           if count == 3 { count = 0}
-       }
-       }
-      
+        didSet {
+            if count == 3 { count = 0}
+        }
+    }
+    
     override var prefersStatusBarHidden: Bool {
-           return true
-       }
-       
+        return true
+    }
+    
     override var shouldAutorotate: Bool {
-           return false
-       }
-       
+        return false
+    }
+    
     // MARK: - Methods
-       
+    
     /// Create Button for control the Device Torch
     private lazy var button: UIButton = {
-           let button = UIButton(type: .system)
-           let widht: CGFloat = 200
-           let height: CGFloat = 50
-           button.frame = CGRect (x: self.view.frame.midX - widht / 2, y: self.view.frame.maxY - self.view.frame.midY/2, width: widht, height: height)
-           button.backgroundColor = .black
-           button.setTitle("Torch on", for: .normal)
-           button.layer.cornerRadius = 25
-           button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-           button.addTarget(nil, action: #selector(buttonAction), for: .touchUpInside)
-           button.tintColor = .white
-           self.view.addSubview(button)
-           return button
-       }()
-
+        let button = UIButton(type: .system)
+        let widht: CGFloat = 200
+        let height: CGFloat = 50
+        button.frame = CGRect (x: self.view.frame.midX - widht / 2, y: self.view.frame.maxY - self.view.frame.midY/2, width: widht, height: height)
+        button.backgroundColor = .black
+        button.setTitle("Torch on", for: .normal)
+        button.layer.cornerRadius = 25
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(nil, action: #selector(buttonAction), for: .touchUpInside)
+        button.tintColor = .white
+        return button
+    }()
+    
     override func viewDidLoad() {
-    super.view.addSubview(button)
-    buttonPressed()
+        super.viewDidLoad()
+        self.view.addSubview(button)
+        buttonPressed()
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-           buttonPressed()
+        buttonPressed()
     }
     
     /// Turn On/Off the Device Torch and Switch the Button Title.
     @objc private func buttonAction() {
-    isRealLightOn.toggle()
-    updateRealLightState()
-
-    let title: String = isRealLightOn ? "Torch off": "Torch on"
-    button.setTitle(title, for: .normal)
+        let title: String = isRealLightOn ? "Torch off": "Torch on"
+        button.setTitle(title, for: .normal)
+        
+        isRealLightOn.toggle()
+        updateRealLightState()
     }
     
     /// Control the Device Torch Status.
     private func updateRealLightState() {
         let device = AVCaptureDevice.default(for: AVMediaType.video)
-               
+        
         if let dev = device, dev.hasTorch {
             do {
                 try dev.lockForConfiguration()
                 dev.torchMode = isRealLightOn ? .on : .off
                 dev.unlockForConfiguration()
-                   } catch {
-                   print(error)
-                   }
-               } else {
-                   print("Device Has No Torch.")
-               }
-           }
+            } catch {
+                print(error)
+            }
+        } else {
+            print("Device Has No Torch.")
+        }
+    }
     
     /// Update the Screen Color
-     @IBAction func buttonPressed() {
+    @IBAction func buttonPressed() {
         switch count {
         case 0:
             view.backgroundColor = .red
@@ -93,5 +94,5 @@ import AVFoundation
             break
         }
         count += 1
-        }
+    }
 }
